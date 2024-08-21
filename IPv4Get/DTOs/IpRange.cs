a978ce759ff0e65f5cc7bb98797ab74618004601
Thiago Piaia <edirlpiaia@gmail.com>
2024-08-21 13:28:00 -0300

    fix bug and adding a DTO

---
+++ IPv4Get/DTOs/IpRange.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPv4Get.DTOs
+{
+    internal struct IpRange
+    {
+        [Index(0)]
+        public uint IpFrom { get; set; }
+
+        [Index(1)]
+        public uint IpTo { get; set; }
+
+        [Index(2)]
+        public string CountryCode { get; set; }
+
+        [Index(3)]
+        public string CountryName { get; set; }
+
+        [Index(4)]
+        public string RegionName { get; set; }
+
+        [Index(5)]
+        public string CityName { get; set; }
+    }
+}
--- IPv4Get/Program.cs
+++ IPv4Get/Program.cs
@@ -1,31 +1,36 @@
 ﻿using System;
 using System.IO;
+using System.IO.Compression;
 using System.Net.Http;
 using System.Threading.Tasks;
 using CsvHelper;
 using System.Globalization;
+using CsvHelper.Configuration;
+using IPv4Get.DTOs;
 
 class Program
 {
     static async Task Main(string[] args)
     {
-        string token = "CqrjUtOHTcE5tug9ZxmOWteYZF3dgxI6AcKPhUQrhsfm4zM9Xfu9R1xrIv7iJXLe";
+        string token = "whHGMIuIZoBodCsIdzE2hdtZN28HPLb3PTppcn4MkmI5SBMZtZSP1qnzFqf58hXd";
         string databaseCode = "DB3LITECSV";
 
         string downloadUrl = $"https://www.ip2location.com/download/?token={token}&file={databaseCode}";
 
-        string filePath = await DownloadFileAsync(downloadUrl, "ips.csv");
+        string zipFilePath = await DownloadFileAsync(downloadUrl, "ips.zip");
 
-        if (File.Exists(filePath))
+        string extractedCsvPath = ExtractCsvFromZip(zipFilePath);
+
+        if (File.Exists(extractedCsvPath))
         {
-            using (var reader = new StreamReader(filePath))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            var ipRanges = ReadIpRanges(extractedCsvPath);
+            int index = 0;
+            foreach (var ipRange in ipRanges)
             {
-                var records = csv.GetRecords<dynamic>(); 
-                foreach (var record in records)
-                {
-                    Console.WriteLine(record); 
-                }
+                Console.WriteLine($"IP From: {ipRange.IpFrom}, IP To: {ipRange.IpTo}, Country: {ipRange.CountryName}, City: {ipRange.CityName}");
+                if (index > 20) return;
+
+                index++;
             }
         }
     }
@@ -37,7 +42,7 @@ class Program
             HttpResponseMessage response = await client.GetAsync(url);
             if (response.IsSuccessStatusCode)
             {
-                var filePath = Path.Combine("../../../", fileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
                 using (var fs = new FileStream(filePath, FileMode.Create))
                 {
                     await response.Content.CopyToAsync(fs);
@@ -50,6 +55,49 @@ class Program
                 Console.WriteLine($"Erro ao baixar o arquivo: {response.StatusCode}");
                 return null;
             }
+        }
+    }
+
+    static string ExtractCsvFromZip(string zipFilePath)
+    {
+        try
+        {
+            string extractPath = Path.Combine(Directory.GetCurrentDirectory(), "extracted");
+            Directory.CreateDirectory(extractPath);
+
+            ZipFile.ExtractToDirectory(zipFilePath, extractPath, overwriteFiles: true);
+            Console.WriteLine($"Arquivo extraÃ­do para: {extractPath}");
+
+            string csvFilePath = Directory.GetFiles(extractPath, "*.csv")[0];
+            return csvFilePath;
+        }
+        catch (InvalidDataException ex)
+        {
+            Console.WriteLine($"Erro ao extrair o arquivo ZIP: {ex.Message}");
+            return "erro";
+        }
+    }
+    public static IEnumerable<IpRange> ReadIpRanges(string csvFilePath)
+    {
+        using (var reader = new StreamReader(csvFilePath))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            csv.Context.RegisterClassMap<IpRangeMap>();
+            var records = csv.GetRecords<IpRange>().ToList();
+            return records;
+        }
+    }
+
+    public sealed class IpRangeMap : ClassMap<IpRange>
+    {
+        public IpRangeMap()
+        {
+            Map(m => m.IpFrom).Index(0);
+            Map(m => m.IpTo).Index(1);
+            Map(m => m.CountryCode).Index(2);
+            Map(m => m.CountryName).Index(3);
+            Map(m => m.RegionName).Index(4);
+            Map(m => m.CityName).Index(5);
         }
     }
 }
